accf65a7055071f7c3643e7f7ec723a9de0c5d3c
Shuman-Olga <shuman.o1979@gmail.com>
2026-08-28 00:05:36 +0700

    change seo

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -7,20 +7,21 @@ export default function SeoPage(props) {
   return (
     <Helmet>
       <title>{data.title}</title>
-      <meta property="og:locale" content="ru_RU" />
-      <meta name="robots" content="index, follow" />
+
       <meta name="description" content={data.description} />
-      <meta name="keywords" content={data.keywords} />
+
+      <meta name="robots" content="index, follow" />
+
       <link rel="canonical" href={data.urlname} />
+
       <meta property="og:type" content="website" />
-      <meta property="og:image" ontent={`/assets/img/${data.img}`}></meta>
-      <meta property="og:image:type" content="image/jpeg" />
-      <meta property="og:image:width" content="350" />
-      <meta property="og:image:height" content="350" />
       <meta property="og:site_name" content="Айболит" />
+      <meta property="og:locale" content="ru_RU" />
+
       <meta property="og:title" content={data.title} />
       <meta property="og:description" content={data.description} />
       <meta property="og:url" content={data.urlname} />
+      <meta property="og:image" content={data.img} />
     </Helmet>
   );
 }
